d5fb52be45a9fffe1ddb9eec7a09208042b2bf47
wen <3424504495@qq.com>
2026-08-04 19:47:30 +0800

    wen

---
+++ 8-4/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<string> name = new List<string> ()
+        {
+            
+            "张三","李四","王五"
+        };
+        //list.AddRange(["Z"]);
+        //list.AddRange([ "q","e","w" ]);
+        //list.Insert(2, "X");
+        //Console.WriteLine(list.Count);
+        //Console.WriteLine(list[4]);
+        Random r = new Random();
+        int mun = r.Next(3);
+        Console.WriteLine(name[mun]);
+        
+    }
+}
+
